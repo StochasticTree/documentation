2c43ff3d0d563ff14fcc79e71ffcd21601b68510
Nicholas Warren <nicholas.warren@austin.utexas.edu>
2024-11-04 14:43:06 -0500

    Updated homepage card content

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -10,38 +10,38 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
+    title: 'StochTree',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        R and Python packages for building stochastic tree ensembles using BCF,
+        BART, XBART, and more.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Arborist App',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Cross-platform app for leveraging the power of StochTree quickly for
+        repeatable analysis.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Case Studies',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Learn how organizations like UT Austin are using StochTree &amp;
+        Arborist to solve real-world problems.
       </>
     ),
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({ title, Svg, description }: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
