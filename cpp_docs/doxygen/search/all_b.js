a136ec2584f8df289ccd58379ee27ce16f6ca32b
andrewherren <andrewherren@users.noreply.github.com>
2025-05-31 01:49:47 +0000

    Deploying to gh-pages from @ StochasticTree/documentation@3d9672217a48923992c145c4d00e0c061241d0dc 🚀

--- cpp_docs/doxygen/search/all_b.js
+++ cpp_docs/doxygen/search/all_b.js
@@ -2,11 +2,15 @@ var searchData=
 [
   ['maxleafdepth_0',['MaxLeafDepth',['../classStochTree_1_1Tree.html#a6abc048b6f39956d15fef71bc5a2efa3',1,'StochTree::Tree']]],
   ['mcmcsampleoneiter_1',['MCMCSampleOneIter',['../group__sampling__group.html#ga08eabd64880e15e5ee94e8ad48110208',1,'StochTree']]],
-  ['model_2',['model',['../group__leaf__model__group.html#gaussian_constant_leaf_model',1,'Gaussian Constant Leaf Model'],['../group__leaf__model__group.html#gaussian_multivariate_regression_leaf_model',1,'Gaussian Multivariate Regression Leaf Model'],['../group__leaf__model__group.html#gaussian_univariate_regression_leaf_model',1,'Gaussian Univariate Regression Leaf Model'],['../group__leaf__model__group.html#inverse_gamma_leaf_model',1,'Inverse Gamma Leaf Model']]],
-  ['model_20api_3',['Leaf Model API',['../group__leaf__model__group.html',1,'']]],
-  ['models_4',['Custom Leaf Models',['../index.html#custom-leaf-models',1,'']]],
-  ['modeltype_5',['ModelType',['../group__leaf__model__group.html#ga8cbfe035085adfd1a05335cf6f543a07',1,'StochTree']]],
-  ['multivariate_20regression_20leaf_20model_6',['Gaussian Multivariate Regression Leaf Model',['../group__leaf__model__group.html#gaussian_multivariate_regression_leaf_model',1,'']]],
-  ['multivariatenormalsampler_7',['MultivariateNormalSampler',['../classStochTree_1_1MultivariateNormalSampler.html',1,'StochTree']]],
-  ['multivariateregressionrandomeffectsmodel_8',['MultivariateRegressionRandomEffectsModel',['../classStochTree_1_1MultivariateRegressionRandomEffectsModel.html',1,'StochTree']]]
+  ['mergeforest_2',['MergeForest',['../classStochTree_1_1TreeEnsemble.html#a7dc289625eee2ff06b8a44958f109c02',1,'StochTree::TreeEnsemble']]],
+  ['mergeforests_3',['MergeForests',['../classStochTree_1_1ForestContainer.html#a8ec1ac6e8569002589e6822e36ddfdf8',1,'StochTree::ForestContainer']]],
+  ['model_4',['model',['../group__leaf__model__group.html#gaussian_constant_leaf_model',1,'Gaussian Constant Leaf Model'],['../group__leaf__model__group.html#gaussian_multivariate_regression_leaf_model',1,'Gaussian Multivariate Regression Leaf Model'],['../group__leaf__model__group.html#gaussian_univariate_regression_leaf_model',1,'Gaussian Univariate Regression Leaf Model'],['../group__leaf__model__group.html#inverse_gamma_leaf_model',1,'Inverse Gamma Leaf Model']]],
+  ['model_20api_5',['Leaf Model API',['../group__leaf__model__group.html',1,'']]],
+  ['models_6',['Custom Leaf Models',['../index.html#custom-leaf-models',1,'']]],
+  ['modeltype_7',['ModelType',['../group__leaf__model__group.html#ga8cbfe035085adfd1a05335cf6f543a07',1,'StochTree']]],
+  ['multiplyforest_8',['MultiplyForest',['../classStochTree_1_1ForestContainer.html#a31d8f33aa0d7072d27d4e81b604c80ac',1,'StochTree::ForestContainer']]],
+  ['multiplyleavesbyvalue_9',['multiplyleavesbyvalue',['../classStochTree_1_1Tree.html#a8564d2929de94ba22cafa6b2bed36b78',1,'StochTree::Tree::MultiplyLeavesByValue()'],['../classStochTree_1_1TreeEnsemble.html#ae50768937738ff9e35190f2360a2e997',1,'StochTree::TreeEnsemble::MultiplyLeavesByValue()']]],
+  ['multivariate_20regression_20leaf_20model_10',['Gaussian Multivariate Regression Leaf Model',['../group__leaf__model__group.html#gaussian_multivariate_regression_leaf_model',1,'']]],
+  ['multivariatenormalsampler_11',['MultivariateNormalSampler',['../classStochTree_1_1MultivariateNormalSampler.html',1,'StochTree']]],
+  ['multivariateregressionrandomeffectsmodel_12',['MultivariateRegressionRandomEffectsModel',['../classStochTree_1_1MultivariateRegressionRandomEffectsModel.html',1,'StochTree']]]
 ];
--- cpp_docs/doxygen/search/functions_a.js
+++ cpp_docs/doxygen/search/functions_a.js
@@ -1,5 +1,9 @@
 var searchData=
 [
   ['maxleafdepth_0',['MaxLeafDepth',['../classStochTree_1_1Tree.html#a6abc048b6f39956d15fef71bc5a2efa3',1,'StochTree::Tree']]],
-  ['mcmcsampleoneiter_1',['MCMCSampleOneIter',['../group__sampling__group.html#ga08eabd64880e15e5ee94e8ad48110208',1,'StochTree']]]
+  ['mcmcsampleoneiter_1',['MCMCSampleOneIter',['../group__sampling__group.html#ga08eabd64880e15e5ee94e8ad48110208',1,'StochTree']]],
+  ['mergeforest_2',['MergeForest',['../classStochTree_1_1TreeEnsemble.html#a7dc289625eee2ff06b8a44958f109c02',1,'StochTree::TreeEnsemble']]],
+  ['mergeforests_3',['MergeForests',['../classStochTree_1_1ForestContainer.html#a8ec1ac6e8569002589e6822e36ddfdf8',1,'StochTree::ForestContainer']]],
+  ['multiplyforest_4',['MultiplyForest',['../classStochTree_1_1ForestContainer.html#a31d8f33aa0d7072d27d4e81b604c80ac',1,'StochTree::ForestContainer']]],
+  ['multiplyleavesbyvalue_5',['multiplyleavesbyvalue',['../classStochTree_1_1TreeEnsemble.html#ae50768937738ff9e35190f2360a2e997',1,'StochTree::TreeEnsemble::MultiplyLeavesByValue()'],['../classStochTree_1_1Tree.html#a8564d2929de94ba22cafa6b2bed36b78',1,'StochTree::Tree::MultiplyLeavesByValue()']]]
 ];
